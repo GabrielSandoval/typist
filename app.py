81fef9191060c621d94a3ca721cc33e18b34dd2a
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2022-09-13 02:41:00 +0800

    Type faster

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ with open('input.txt') as f:
     for line in lines:
         print(line)
         for word in line.split():
-            pyautogui.typewrite(list(word), interval=random_between(0.025, 0.05))
-            time.sleep(random_between(0.05, 0.1))
+            pyautogui.typewrite(list(word), interval=random_between(0.01, 0.03))
+            time.sleep(random_between(0.03, 0.05))
             pyautogui.typewrite(' ')
         pyautogui.typewrite('\n')
-        time.sleep(random_between(0.4, 1))
+        time.sleep(random_between(0.1, 0.5))
